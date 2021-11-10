1a2f563c8982e3a45ab12f620f8004d8c7423553
Andy Lum <andlum@ut.utm.edu>
2021-11-09 19:56:33 -0600

    Third commit: adding updated main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
 using namespace std;
+int sum(int n);
+
 
 int main(){
-  cout << "Hello World!";
+  cout << "Hello World!" << endl;
+  int n;
+  cout << "Select a random number."<< endl;
+  cin >> n;
+
+  sum(n);
+  cout << n << endl;
   return 0;
+}
+int sum(int n){
+  int sums = 1;
+  sums = sums + n;
+
+  return sums;
 }
